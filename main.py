5e34fa932662d4a9f4fef1b2fb9e527cac84b5a3
LanRuo <53806646+Ruohan-Yang@users.noreply.github.com>
2023-12-03 21:45:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+
+import torch
+import argparse
+import datetime
+
+from src.Load import load_data
+from src import AMMN
+from src.Train import train_AMMN
+
+import warnings
+warnings.filterwarnings("ignore")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='AMMN', help='name of model')
+    parser.add_argument('--dataset', '-d', type=str, default='baby', help='name of dataset')
+    parser.add_argument('--proportion', '-p', type=float, default=0.5, help='uncertain missing proportion')
+    parser.add_argument('--learning_rate', '-l', type=float, default=0.01, help='initial learning rate')
+    parser.add_argument('--epochs', '-e', type=int, default=50, help='number of iterations')
+    parser.add_argument('--batch_size', '-b', type=int, default=512, help='batch size of train data')
+    parser.add_argument('--dim_node', '-dim', type=int, default=32, help='dims of embedding')
+    parser.add_argument('--epsilon', '-epsilon', type=int, default=8, help='epsilon of PGD')
+    parser.add_argument('--alpha', '-alpha', type=int, default=4, help='alpha of PGD')
+    parser.add_argument('--t', '-t', type=int, default=3, help='number of disturbances of PGD')
+    parser.add_argument('--repeats', '-r', type=int, default=1, help='number of repeats')
+    parser.add_argument('--outfile', '-f', type=str, default='log', help='record file')
+
+    args = parser.parse_args()
+    print('The program starts running.')
+
+    for repeat in range(args.repeats):
+        print('***************************')
+        begin = datetime.datetime.now()
+        print('Start time ', begin)
+        time = str(begin.year) + '-' + str(begin.month) + '-' + str(begin.day) + '-' + str(begin.hour) + '-' + str(
+            begin.minute) + '-' + str(begin.second)
+        args.outfile = 'log/' + args.dataset + '-' + time + '.txt'
+        print(args)
+
+        outfile = open(args.outfile, 'a', encoding='utf-8')
+        write_infor = "\nproportion:{}, lr:{}, epoch:{}, batch:{}, dim:{}, epsilon:{}, alpha:{}, t:{}".format(args.proportion, args.learning_rate, args.epochs, args.batch_size, args.dim_node, args.epsilon, args.alpha, args.t)+'\n'
+        outfile.write(write_infor)
+        outfile.close()
+
+        # load data
+        num_user, num_item, missing_case_count, origin_dims, gcn_data, train_data, val_data, test_data = load_data(
+            args.dataset, args.batch_size, args.proportion)
+        # load model
+        my_model = AMMN.AMMN_Net(origin_dims, args.dim_node, missing_case_count, num_user, num_item, gcn_data)
+        # run model
+        if torch.cuda.is_available():
+            my_model = my_model.cuda()
+            gcn_data.x = gcn_data.x.cuda()
+            gcn_data.edge_index = gcn_data.edge_index.cuda()
+        train_AMMN(args.model, train_data, val_data, test_data, my_model, args.epochs, args.learning_rate, args.outfile, args.epsilon, args.alpha, args.t)
+
+        # time
+        end = datetime.datetime.now()
+        print('End time ', end)
+        print('Run time ', end-begin)
---
+++ src/Train.py
@@ -0,0 +1,115 @@
+
+import os
+import shutil
+import torch
+from torch import nn
+import numpy as np
+from src.AMMN import PGD
+from torch.autograd import Variable
+
+
+def train_AMMN(model_name, train_data, val_data, test_data, model, epochs, initial_learning_rate, outfile, epsilon, alpha, t):
+    outfile = open(outfile, 'a', encoding='utf-8')
+    model_path = 'save/' + model_name + '/'
+    if os.path.exists(model_path):  # 清除之前运行代码生成的模型
+        shutil.rmtree(model_path)
+    if not os.path.exists(model_path):
+        os.mkdir(model_path)
+    best_valid_acc = 0
+    best_valid_dir = ''
+    pgd = PGD(model, 'trans_gcn', epsilon=epsilon, alpha=alpha)
+    K = t  # 扰动次数
+    criterion = nn.CrossEntropyLoss()
+    print('Training...')
+    for epoch in range(epochs + 1):
+        p = epoch / epochs
+        learning_rate = initial_learning_rate / pow((1 + 10 * p), 0.75)
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  # weight_decay=0.0001
+        # 训练
+        model.train()
+        loss_d_vec = []
+        loss_p_vec = []
+        loss_final_vec = []
+        train_acc_vec = []
+        for data in train_data:
+            users_tensor, items_tensor, modality_labels, link_labels = data
+            if torch.cuda.is_available():
+                users_tensor = users_tensor.cuda()
+                items_tensor = items_tensor.cuda()
+                modality_labels = modality_labels.cuda()
+                link_labels = link_labels.cuda()
+            # tensor不能反向传播，variable可以反向传播
+            users_tensor = Variable(users_tensor)
+            items_tensor = Variable(items_tensor)
+            modality_labels = torch.squeeze(Variable(modality_labels))
+            link_labels = torch.squeeze(Variable(link_labels))
+            # 前向传播
+            pred_links, pred_trans = model(users_tensor, items_tensor)
+            # 计算损失
+            loss_p = criterion(pred_links, link_labels)
+            loss_d = criterion(torch.squeeze(pred_trans), modality_labels)
+            loss_final = loss_p + loss_d
+
+            loss_d_vec.append(loss_d.cpu().detach().numpy())
+            loss_p_vec.append(loss_p.cpu().detach().numpy())
+            loss_final_vec.append(loss_final.cpu().detach().numpy())
+
+            _, argmax = torch.max(pred_links, 1)
+            batch_acc = (argmax == link_labels).float().mean()
+            train_acc_vec.append(batch_acc.item())
+
+            # 正常训练 反向传播 得到正常的梯度
+            loss_final.backward()
+            # 对抗训练
+            pgd.backup_grad()  # 备份模型全部梯度
+            for t in range(K):
+                pgd.attack(is_first_attack=(t == 0))  # first attack时备份模型全部参数
+                # 在embedding上添加对抗扰动, 对抗部分的参数进行扰动更新, 包括备份也更新
+                if t != K - 1:
+                    optimizer.zero_grad()  # 模型全部梯度置0
+                else:  # 最后一次扰动
+                    pgd.restore_grad()  # 恢复模型全部梯度 即对抗前计算的梯度
+                # 计算加入扰动后的新损失与新梯度
+                _, adv_pred_d = model(users_tensor, items_tensor)
+                loss_adv = criterion(torch.squeeze(adv_pred_d), modality_labels)
+                loss_adv.backward()  # 反向传播，在原始正常的梯度基础上，逐次累加对抗训练K次扰动的梯度
+            pgd.restore()   # 恢复对抗部分参数 原始参数 对抗部分新梯度
+            # 至此 模型参数没有变化 对抗部分新梯度
+            optimizer.step()  # 梯度下降算法，更新参数
+            optimizer.zero_grad()  # 清空batch梯度,不影响下一batch计算
+
+        loss_d = np.mean(loss_d_vec)
+        loss_p = np.mean(loss_p_vec)
+        loss_final = np.mean(loss_final_vec)
+        train_acc = np.mean(train_acc_vec)
+        write_infor = "epoch:[{}/{}], lr:{:.4f}, loss_final:{:.4f}, loss_p:{:.4f}, loss_d:{:.4f}".format(epoch, epochs, learning_rate, loss_final, loss_p, loss_d)
+        print(write_infor)
+        outfile.write('\n'+write_infor)
+
+        # 验证
+        model.eval()
+        valid_acc, valid_pre, valid_auc = model.metrics_eval(val_data)
+        write_infor = "Train acc:{:.4f}, Valid acc:{:.4f}, Valid pre:{:.4f}, Valid auc:{:.4f} ".format(train_acc, valid_acc, valid_pre, valid_auc)
+        print(write_infor)
+        outfile.write('\n'+write_infor)
+
+        # 保存最好模型
+        if valid_acc > best_valid_acc:
+            best_valid_acc = valid_acc
+            best_valid_dir = model_path + 'model' + str(epoch) + '.pkl'
+            torch.save(model, best_valid_dir)
+
+    # 加载最好模型 测试
+    model.eval()
+    print('Load best model ...')
+    write_infor = 'Best model: ' + best_valid_dir
+    print(write_infor)
+    outfile.write('\n\n' + write_infor)
+    model = torch.load(best_valid_dir)
+    acc, pre, auc = model.metrics_eval(test_data)
+    write_infor = "Test acc:{:.4f}, pre:{:.4f}, auc:{:.4f}".format(acc, pre, auc)
+    print(write_infor)
+    outfile.write('\n'+write_infor)
+    outfile.close()
+
+
